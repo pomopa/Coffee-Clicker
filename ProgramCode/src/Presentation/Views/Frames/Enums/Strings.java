45c07d72ba98e103ba015d78db85d538962a4da2
Pol Monne Parera <152496991+pomopa@users.noreply.github.com>
2025-12-15 08:34:33 -0600

    Update Strings enum to English translations

--- ProgramCode/src/Presentation/Views/Frames/Enums/Strings.java
+++ ProgramCode/src/Presentation/Views/Frames/Enums/Strings.java
@@ -7,197 +7,197 @@ public enum Strings {
     /**
      * Welcome message displayed to users.
      */
-    WELCOME("Benvingut al Coffee Clicker"),
+    WELCOME("Welcome to Coffee Clicker"),
 
     /**
      * Login option text.
      */
-    LOGIN("Iniciar sessió"),
+    LOGIN("Log in"),
 
     /**
      * Recover password text.
      */
-    RECOVER_PASSWORD("Recuperar contrasenya"),
+    RECOVER_PASSWORD("Recover Password"),
 
     /**
      * Recover password text.
      */
-    REQUEST_PASSWORD("Solicitar contrasenya"),
+    REQUEST_PASSWORD("Request Password"),
 
     /**
      * Recover password text.
      */
-    MESSAGE_PASSWORD("La vostra contrasenya és: "),
+    MESSAGE_PASSWORD("Your password is: "),
 
     /**
      * Recover password text.
      */
-    NO_USER_IN_DATABASE("No existeix aquest usuari a la base de dades. Si us plau, prova de nou"),
+    NO_USER_IN_DATABASE("The user does not exist in the database. Please, try again"),
 
     /**
      * Restore password option text.
      */
-    RESTORE_PASS("Recuperar contrasenya"),
+    RESTORE_PASS("Restore password"),
 
     /**
      * Register option text.
      */
-    REGISTER("Registrar-se"),
+    REGISTER("Register"),
 
     /**
      * Label for username field.
      */
-    USERNAME("Nom d'usuari:"),
+    USERNAME("Username:"),
 
     /**
      * Label for username or mail field.
      */
-    USERNAME_MAIL("Nom d'usuari o mail:"),
+    USERNAME_MAIL("Username or email:"),
 
     /**
      * Label for new game name field.
      */
-    GAME_NAME("Nom de la nova partida:"),
+    GAME_NAME("New game name:"),
 
     /**
      * Label for email field.
      */
-    EMAIL("Correu electrònic:"),
+    EMAIL("Email:"),
 
     /**
      * Format hint for email field.
      */
-    EMAIL_FORMAT("* Inclou @ i domini (.com/.cat/.es/etc)"),
+    EMAIL_FORMAT("* Includes @ and domain (.com/.cat/.es/etc)"),
 
     /**
      * Format hint for password field.
      */
-    PASSWORD_FORMAT("* Inclou almenys 8 caràcters alfanumèrics"),
+    PASSWORD_FORMAT("* includes at least 8 alphanumerical characters"),
 
     /**
      * Label for password field.
      */
-    PASSWORD("Contrasenya:"),
+    PASSWORD("Password:"),
 
     /**
      * Label for confirm password field.
      */
-    CONFIRM_PASSWORD("Confirma contrasenya:"),
+    CONFIRM_PASSWORD("Confirm password:"),
 
     /**
      * Error message for when the timer ends.
      */
-    TIMER_ENDED("Ha trigat massa a interectuar, si desitja realitzar alguna acció prem algun botó de nou"),
+    TIMER_ENDED("You took to long to take action, if you want to do something press any button again"),
 
     /**
      * Error message for incomplete fields.
      */
-    ERROR_PLEASE_COMPLETE_ALL_FIELDS("Si us plau, completa tots els camps."),
+    ERROR_PLEASE_COMPLETE_ALL_FIELDS("Please, fill in all the fields."),
 
     /**
      * Prompt for entering new user data.
      */
-    ENTER_NEW_USER_DATA("Introdueix les dades del teu nou usuari"),
+    ENTER_NEW_USER_DATA("Insert your username"),
 
     /**
      * Message for when the user has no open game.
      */
-    YOU_HAVE_NO_OPEN_GAME("Estas preparat per jugar :)"),
+    YOU_HAVE_NO_OPEN_GAME("Are you ready to play? :)"),
 
     /**
      * Message for when the user has an open game.
      */
-    YOU_HAVE_AN_OPEN_GAME("Tens una partida oberta"),
+    YOU_HAVE_AN_OPEN_GAME("You have an open game"),
 
     /**
      * Button text for continuing a game.
      */
-    CONTINUE_GAME("Continuar partida"),
+    CONTINUE_GAME("Continue game"),
 
     /**
      * Error message for trying to anger a coffee.
      */
-    CANT_ANGER_A_COFFEE("No pots enfadar a un cafè"),
+    CANT_ANGER_A_COFFEE("You can not anger a coffee"),
 
     /**
      * Confirmation message for deleting an account.
      */
-    CONFIRMATION_DELETE_ACCOUNT("Segur que vols eliminar el compte?"),
+    CONFIRMATION_DELETE_ACCOUNT("Are you sure you want to delete your account?"),
 
     /**
      * Confirmation message for logging out.
      */
-    CONFIRMATION_LOG_OUT("Segur que vols tancar sessió?"),
+    CONFIRMATION_LOG_OUT("Are you sure you want to close your session?"),
 
     /**
      * Confirmation option text.
      */
-    CONFIRM("Estic segur"),
+    CONFIRM("I am sure"),
 
     /**
      * Non-confirmation option text.
      */
-    NOT_CONFIRM("No estic segur"),
+    NOT_CONFIRM("I am not sure"),
 
     /**
      * Button text for viewing statistics.
      */
-    VIEW_STATISTICS("Veure estadístiques"),
+    VIEW_STATISTICS("View statistics"),
 
     /**
      * Button text for starting a game.
      */
-    START_GAME("Començar a jugar"),
+    START_GAME("Start playing"),
 
     /**
      * Button text for deleting a user.
      */
-    DELETE_USER("Eliminar el compte"),
+    DELETE_USER("Delete account"),
 
     /**
      * Title for power-ups section.
      */
-    POWER_UPS_TITLE("MILLORES"),
+    POWER_UPS_TITLE("POWER-UPS"),
 
     /**
      * Title for power-ups  GENERADORS.
      */
-    GENERATORS_TITLE("GENERADORS"),
+    GENERATORS_TITLE("GENERATORS"),
 
     /**
      * Advisory message for deleting a user.
      */
-    DELETE_USER_ADVISE("(!) Si s'elimina el compte del sistema les dades d'aquest perfil no es podran recuperar mai (!)"),
+    DELETE_USER_ADVISE("(!) If you delete the account from the system you will never be able to recover the data (!)"),
 
     /**
      * Logout option text.
      */
-    LOGOUT("Tanca sessió"),
+    LOGOUT("Close session"),
 
     /**
      * Profile management option text.
      */
-    PROFILE("Gestió del compte"),
+    PROFILE("Profile management"),
 
     /**
      * Button text for buying something.
      */
-    BUY("Comprar"),
+    BUY("Buy"),
 
     /**
      * Error message for not having a generator.
      */
-    NO_GENERATOR("Per a comprar una millora has de tenir el generador al que afecta!"),
+    NO_GENERATOR("To buy an upgrade you must have the generators it affects!"),
 
     /**
      * Error message for not having enough coffees.
      */
-    NOT_ENOUGH_COFFEES("No té suficients cafés per a comprar la millora!"),
+    NOT_ENOUGH_COFFEES("You don't have enough coffees to buy the upgrade!"),
 
     /**
      * Button text for ending a game.
      */
-    END_GAME("Finalitzar partida");
+    END_GAME("End game");
 
     private final String value;
 
